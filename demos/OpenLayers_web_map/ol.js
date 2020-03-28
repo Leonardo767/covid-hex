b80253c703eccae6ec1324a0a14aba11ee732db4
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2020-03-28 13:25:20 -0500

    removed bad demos

--- demos/OpenLayers_web_map/ol.js
+++
@@ -1,8 +0,0 @@
-var apiKey = "p7o8SpaIuYP1Tf5LYh0p";
-
-olms(
-  "map",
-  "https://maps.tilehosting.com/styles/basic/style.json?key=" + apiKey
-).then(function(map) {
-  // do anything with the passed `ol.Map` instance here.
-});
