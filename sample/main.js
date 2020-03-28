f5b95c1c0ff1c0d33147c74495ead7783e3f8f6e
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2020-03-28 16:59:12 -0500

    simplified folders

---
+++ sample/main.js
@@ -0,0 +1,12 @@
+// mapboxgl.accessToken = api_config.KEY_MAPBOX;
+
+// var map = new mapboxgl.Map({
+//   container: "map",
+//   style: "mapbox://styles/mapbox/light-v10",
+//   center: [0, 0],
+//   zoom: 2
+// });
+
+// map.on("load", function(e) {
+//   /* Add the data to your map as a layer */
+// });
