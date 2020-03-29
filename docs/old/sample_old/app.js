e75e223dbe17468ef723f9b3b58f7d0910888b37
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2020-03-29 12:16:33 -0500

    redid folders for React

--- docs/old/sample_old/app.js
+++ docs/old/sample_old/app.js
@@ -1,6 +1,4 @@
 const api_config = require("../api_config");
-// mapboxgl = require("mapbox-gl");
-// mapboxgl.accessToken = api_config.KEY_MAPBOX;
 mapboxgl = async function() {
   let mapboxgl = await require("mapbox-gl@0.43.0");
   mapboxgl.accessToken = api_config.KEY_MAPBOX;
@@ -8,5 +6,4 @@ mapboxgl = async function() {
 };
 h3 = require("h3-js");
 geojson2h3 = require("geojson2h3");
-
-console.log(geojson2h3);
+console.log("Successfully imported modules!");
--- docs/old/sample_old/server.js
+++ docs/old/sample_old/server.js
@@ -1,7 +1,8 @@
 const http = require("http");
 const app = require("./app");
 
-const port = process.env.POR || 8000;
+const port = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 server.listen(port);
+console.log("Listening on port " + port + "...");
