5b09dcbd71d3a5f2d82b04aad874f1cfd75d4136
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2020-03-30 12:27:21 -0500

    Added sample objects to render

---
+++ src/layers.js
@@ -0,0 +1,59 @@
+import { geoToH3, kRing } from "h3-js";
+// @ts-ignore
+import geojson2h3 from "geojson2h3";
+// geojson2h3 = require("https://bundle.run/geojson2h3");
+
+export const layer_example = {
+  id: "point",
+  type: "circle",
+  paint: {
+    "circle-radius": 10,
+    "circle-color": "#007cbf"
+  }
+};
+
+export const geojson_example = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: { type: "Point", coordinates: [-122.4, 37.8] }
+    }
+  ]
+};
+
+// ======================================================
+// GENERATE HEX GEOJSON
+
+function create_hexagons(lon, lat) {
+  const centerHex = geoToH3(lon, lat, 8);
+  const kRing_res = kRing(centerHex, 3);
+  // Reduce hexagon list to a map with random values
+  return kRing_res.reduce(
+    (res, hexagon) => ({ ...res, [hexagon]: Math.random() }),
+    {}
+  );
+}
+
+function create_hex_geojson(hexagons) {
+  const geojson = geojson2h3.h3SetToFeatureCollection(
+    Object.keys(hexagons),
+    hex => ({
+      value: hexagons[hex]
+    })
+  );
+  console.log("returning geojson hex...");
+  return geojson;
+}
+
+export const hex_geojson = create_hex_geojson(create_hexagons(-122.4, 37.8));
+
+export const hex_layer = {
+  id: "h3-hexes-layer",
+  source: "h3-hexes",
+  type: "fill",
+  interactive: false,
+  paint: {
+    "fill-outline-color": "rgba(255,0,0,0.5)"
+  }
+};
--- src/map.js
+++ src/map.js
@@ -1,5 +1,11 @@
-import React, { useState, Component } from "react";
-import ReactMapGL, { Layer } from "react-map-gl";
+import React, { Component } from "react";
+import ReactMapGL, { Source, Layer } from "react-map-gl";
+import {
+  layer_example,
+  geojson_example,
+  hex_geojson,
+  hex_layer
+} from "./layers";
 
 class Map extends Component {
   constructor(props) {
@@ -14,12 +20,14 @@ class Map extends Component {
       height: "100vh",
       zoom: this.zoom0
     };
+    // initialize viewport
     this.state = {
       viewport: viewport
     };
   }
 
   setViewport(viewport) {
+    // updates viewport upon panning, scrolling, etc.
     this.setState({ viewport: viewport });
   }
 
@@ -27,25 +35,21 @@ class Map extends Component {
     return (
       <ReactMapGL
         {...this.state.viewport}
+        // specify API token and controller for ReactMapGL
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
         onViewportChange={viewport => {
           this.setViewport(viewport);
         }}
-      ></ReactMapGL>
+      >
+        {/* <Source id="my-data" type="geojson" data={geojson_example}>
+          <Layer {...layer_example} />
+        </Source> */}
+        <Source id="h3-hexes" type="geojson" data={hex_geojson}>
+          <Layer {...hex_layer} />
+        </Source>
+      </ReactMapGL>
     );
   }
 }
 
 export default Map;
-
-// const [viewport, setViewport] = useState({
-//   latitude: 32,
-//   longitude: -97,
-//   width: "100vw",
-//   height: "100vh",
-//   zoom: 7
-// });
-
-// console.log(create_hexagons(-122.4, 37.7923539));
-
-// return <div></div>;
