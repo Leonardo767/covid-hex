82a4064f1f4e5059d1716c9a8c2f42aa915eb923
Leonardo Pinero <39559337+Leonardo767@users.noreply.github.com>
2020-03-30 05:16:55 -0500

    separating Interactive Map (does not render)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React, { useState } from "react";
-import ReactMapGL from "react-map-gl";
+import React, { useState, Component } from "react";
+import ReactMapGL, { Layer } from "react-map-gl";
 import { hexagons } from "./data/hex-SanFran.js";
 import "./App.css";
 // global.document = {};
 // import logo from "./logo.svg";
 // import * as parkData from "./data/skateboard-park.json";
+
+class Map extends Component {
+  render() {
+    return (
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+        onViewportChange={viewport => {
+          setViewport(viewport);
+        }}
+      ></ReactMapGL>
+    );
+  }
+}
 
 function App() {
   const [viewport, setViewport] = useState({
@@ -15,19 +29,9 @@ function App() {
     zoom: 7
   });
 
-  // console.log(hexagons);
+  console.log(hexagons());
 
-  return (
-    <div>
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        onViewportChange={viewport => {
-          setViewport(viewport);
-        }}
-      ></ReactMapGL>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
