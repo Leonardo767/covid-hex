7dbdc2b4740df534bc43300b3974784703eaba21
jbd95 <james.brady2@mavs.uta.edu>
2020-04-08 18:37:02 -0500

    change map to class component

--- src/map.js
+++ src/map.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, createRef } from "react";
 import ReactMapGL, {
   FlyToInterpolator,
   WebMercatorViewport
@@ -8,17 +8,26 @@ import Search from "./components/Search";
 import HexRender from "./layers";
 import { Point } from "./api/mapbox"; // eslint-disable-line
 
-function Map() {
+class Map extends React.Component {
   // const {} = props;
-  const [viewport, setViewport] = useState({
-    latitude: 33.9137,
-    longitude: -98.4934,
-    width: "100vw",
-    height: "100vh",
-    zoom: 7
-  });
-  const [location, setLocation] = useState(null);
-  const _map = useRef(null);
+  constructor(props) {
+    super(props)
+    this._map = createRef()
+    this.state = {
+      viewport: {
+        latitude: 33.9137,
+        longitude: -98.4934,
+        width: "100vw",
+        height: "100vh",
+        zoom: 7
+      },
+      location: null
+    }
+  }
+
+  setViewport = (viewport) => {
+    this.setState({viewport})
+  }
 
   /**
    *
@@ -27,58 +36,67 @@ function Map() {
    * @param {number} latitude
    * @param {Point[]} boundingBox
    */
-  const onSearchChange = (
+  onSearchChange = (
     location,
     longitude,
     latitude,
     boundingBox = null
   ) => {
-    setLocation(location);
+    console.log(boundingBox)
+    console.log(latitude)
+    this.setState({location});
 
     // Most features have a bounding box but specific locations do not so give them the highest zoom possible
     if (boundingBox) {
       const { longitude, latitude, zoom } = new WebMercatorViewport(
-        viewport
+        this.state.viewport
       ).fitBounds([boundingBox[0].toArray(), boundingBox[1].toArray()], {
         padding: 20,
         offset: [0, -100]
       });
 
-      setViewport(viewport => ({
-        ...viewport,
-        latitude,
-        longitude,
-        zoom,
-        transitionDuration: 5000,
-        transitionInterpolator: new FlyToInterpolator(),
-        transitionEasing: easeCubic
-      }));
+      this.setState({
+        viewport: {
+          ...this.state.viewport,
+          latitude,
+          longitude,
+          zoom,
+          transitionDuration: 5000,
+          transitionInterpolator: new FlyToInterpolator(),
+          transitionEasing: easeCubic
+        }
+      })
+
     } else {
-      setViewport(viewport => ({
-        ...viewport,
-        latitude,
-        longitude,
-        zoom: 18, // give highest zoom possible
-        transitionDuration: 5000,
-        transitionInterpolator: new FlyToInterpolator(),
-        transitionEasing: easeCubic
-      }));
+      this.setState({
+        viewport: {
+          ...this.state.viewport,
+          latitude,
+          longitude,
+          zoom: 18, // give highest zoom possible
+          transitionDuration: 5000,
+          transitionInterpolator: new FlyToInterpolator(),
+          transitionEasing: easeCubic
+        }
+      })
     }
   };
 
-  return (
-    <div>
-      <ReactMapGL
-        ref={_map}
-        {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        onViewportChange={setViewport}
-      >
-        <HexRender viewport={viewport}></HexRender>
-      </ReactMapGL>
-      <Search location={location} onChange={onSearchChange} />
-    </div>
-  );
+  render() {
+    return (
+      <div>
+        <ReactMapGL
+          ref={this._map}
+          {...this.state.viewport}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          onViewportChange={this.setViewport}
+        >
+          <HexRender viewport={this.state.viewport}></HexRender>
+        </ReactMapGL>
+        <Search location={this.state.location} onChange={this.onSearchChange} />
+      </div>
+    );
+  }
 }
 
 export default Map;
